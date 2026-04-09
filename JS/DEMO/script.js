ca06e55826fc87f255d11ea76a3b57c64beed892
akp03112000 <akp03112000@gmail.com>
2026-04-09 12:15:03 +0530

    Add initial HTML and JavaScript files for demo and practice exercises

---
+++ JS/DEMO/script.js
@@ -0,0 +1,15 @@
+let a = 20
+
+console.log(a)
+
+
+let b;
+
+console.log(c)
+
+var c;
+
+console.log(c)
+
+console.log(d)
+let d = 20
---
+++ JS/Day 1/Practise/script1.js
@@ -0,0 +1,10 @@
+
+let a =Number (prompt("Enter a number: "));
+if(a < 8){
+    for(let i = 1; i < a; i++){
+        console.log("It is " + a);
+    }   
+}else {
+    for(let i = 1; i < a*a; i++){
+}        console.log("It is " + a);
+}
